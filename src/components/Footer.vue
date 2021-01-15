1c961b1bdf86a7a21105fbe47d0a3e4796d57318
Léna <lena.clavier@gmail.com>
2021-01-15 19:01:13 +0100

    add confidentialite

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,6 @@
     <div class="fond h-40 md:h-56 lg:h-56 flex flex-col justify-end text-white">
         <ul class="flex flex-col md:flex-row lg:flex-row justify-center h-auto md:h-16 lg:h-16">
             <li><router-link to="/mentionslegales">Mentions légales</router-link></li>
-            <li><router-link to="/politiquecookies">Politique en matière de cookies</router-link></li>
             <li><router-link to="/politiqueconfidentialite">Politique de confidentialité</router-link></li>
         </ul>
         <p class="text-center my-2 h-auto md:h-8 lg:h-8">© 2021 par Urbanoo</p>
--- src/router.js
+++ src/router.js
@@ -2,13 +2,11 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import LandingView from './views/LandingView.vue'
 import MentionsView from './views/MentionsView.vue'
-import CookiesView from './views/CookiesView.vue'
 import ConfidentialiteView from './views/ConfidentialiteView.vue'
 
 const routes = [
   { path: '/', component: LandingView },
   { path: '/mentionslegales', component: MentionsView },
-  { path: '/politiquecookies', component: CookiesView },
   { path: '/politiqueconfidentialite', component: ConfidentialiteView },
 ]
 
