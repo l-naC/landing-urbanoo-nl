5dc0af0f9b0337c4169515b40f3ceb9e0c03e40a
Léna <lena.clavier@gmail.com>
2021-01-15 17:54:45 +0100

    Corrections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
         <div class="w-full md:w-3/5 lg:w-3/5 flex flex-col justify-center text-left">
             <div class="w-10/12 md:w-1/2 lg:w-1/2 mx-auto mb-0 md:mb-60 lg:mb-60">
                 <h1 class="text-2xl font-black my-5">URBANOO</h1>
-                <h2 class="text-xl font-medium my-8">Aplication pour visitez une ville différement à travers des parcours intéractif</h2>
+                <h2 class="text-xl font-medium my-8">Application pour visiter une ville différemment à travers des parcours interactifs</h2>
                 <p class="text-xl font-normal my-8">Abonnez-vous et tenez-vous au courant de sa sortie.</p>
                 <!-- Begin Mailchimp Signup Form -->
                     <form action="https://gmail.us7.list-manage.com/subscribe/post?u=fd4540943b03621f8a372dc3e&amp;id=adaa40922b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
@@ -18,9 +18,9 @@
                     </form>
                 <!--End mc_embed_signup-->
                 <div class="flex my-8">
-                    <img class="img-rs" src="../assets/images/facebook.svg" alt="facebook">
-                    <img class="img-rs" src="../assets/images/instagram.svg" alt="instagram">
-                    <img class="img-rs" src="../assets/images/pinterest.svg" alt="instagram">
+                    <a href="https://www.facebook.com/UrbanooApp"><img class="img-rs" src="../assets/images/facebook.svg" alt="facebook"></a>
+                    <!-- <a href=""><img class="img-rs" src="../assets/images/instagram.svg" alt="instagram"></a> -->
+                    <a href="https://twitter.com/Urbanoo9"><img class="img-rs" src="../assets/images/twitter.svg" alt="twitter"></a>
                 </div>
             </div>
         </div>
